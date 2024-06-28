daa2e9ce060ef6cc91c74fa6b6bdae24cb5b7d02
sasaki1005 <s1310011@u-aizu.ac.jp>
2024-06-29 06:12:08 +0900

    HoT: initial revision

---
+++ HT.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int i,co,t=0,h=0;
+  char str[20];
+
+
+  printf("Tossing a coin...\n");
+
+  for(i=0;i<3;i++){
+    printf("Round %d:",i);
+    co=rand()%10+1;
+    if(co<5){
+      printf(" Heads\n");
+      h++;
+    }
+    else{
+      printf(" Tails\n");
+      t++;
+    }
+  }
+  printf("Heads: %d, Tails: %d\n",h,t);
+
+    
+    
+    
+  return 0;
+}
